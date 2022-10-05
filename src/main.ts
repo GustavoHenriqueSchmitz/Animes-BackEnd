b9a97ef52ddaa16aae53d56eb836c2846ff9b95d
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2022-10-05 10:06:16 -0300

    initing type-script

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-console.log('hello world!');
+import express from 'express';
+import {Express} from 'express';
+
+const app: Express = express();
+
+app.use(express.json());
+
+app.listen(5000, () => {
+  console.log('Servidor inicializado na porta 5000');
+});
